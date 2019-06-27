187ba236e4238819cc7dd44b133773d7d1cd8aa4
Bruno Igreja <bigreja+github@gmail.com>
2019-06-27 18:04:00 +0100

    inicial

---
+++ src/Extensions/WaterMarkExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace bigreja\watermarkimage\Extensions;
+
+use SilverStripe\Core\Extension;
+class WaterMarkExtension extends Extension
+{
+    public function Square($width)
+    {
+        $variant = $this->owner->variantName(__FUNCTION__, $width);
+        return $this->owner->manipulateImage($variant, function (\SilverStripe\Assets\Image_Backend $backend) use($width) {
+            $clone = clone $backend;
+            $resource = clone $backend->getImageResource();
+            $resource->fit($width);
+            $clone->setImageResource($resource);
+            return $clone;
+        });
+    }
+
+    public function Blur($amount = null)
+    {
+        $variant = $this->owner->variantName(__FUNCTION__, $amount);
+        return $this->owner->manipulateImage($variant, function (\SilverStripe\Assets\Image_Backend $backend) use ($amount) {
+            $clone = clone $backend;
+            $resource = clone $backend->getImageResource();
+            $resource->blur($amount);
+            $clone->setImageResource($resource);
+            return $clone;
+        });
+    }
+
+    public function WaterMark($amount = null)
+    {
+        $variant = $this->owner->variantName(__FUNCTION__, $amount);
+        return $this->owner->manipulateImage($variant, function (\SilverStripe\Assets\Image_Backend $backend) use ($amount) {
+            $clone = clone $backend;
+            $resource = clone $backend->getImageResource();
+            $resource->insert('themes/superbraga/images/logo/sob-sb.png');
+            $clone->setImageResource($resource);
+            return $clone;
+        });
+    }
+
+}
---
+++ src/Extensions/watermarkimagesettings.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Add field for member image on profile!
+ *
+ */
+namespace bigreja\watermarkimage\Extensions;
+
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Assets\Image;
+use SilverStripe\Assets\File;
+use SilverStripe\Forms\FieldList;
+
+class watermarkimagesettings extends DataExtension
+{
+
+    private static $db = [
+    ];
+
+	private static $has_one = array(
+		'watermarkimg' =>  Image::class,
+	);
+
+	private static $owns = [
+		'watermarkimg'
+	];
+
+
+
+	private static $summary_fields = array(
+		'GridThumbnail' => 'Profile Image'
+	);
+
+   public function updateCMSFields(FieldList $fields)
+{
+
+        $image = new UploadField('watermarkimg', 'setings');
+        $image->setFolderName('watermarkimg');
+        $image->getValidator()->setAllowedExtensions(['png','gif','jpeg','jpg']);
+
+
+        $fields->addFieldsToTab('Root.WatermarkSettings', $image);
+    }
+
+
+public function getGridThumbnail()
+    {
+        // $this->owner refers to the original instance. In this case a `Member`.
+
+        if($this->owner->watermarkimg()->exists()) {
+            return $this->owner->watermarkimg()->ScaleMaxHeight(50);
+        }
+
+        return "(no image)";
+    }
+
+public function onAfterWrite()
+{
+    parent::onAfterWrite();
+    if ($this->owner->watermarkimgID) {
+        $this->owner->watermarkimg()->publishSingle();
+    }
+}
+
+}
