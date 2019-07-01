7131c4095d5d9f615fd392046d641584c4f592e0
Bruno Igreja <bigreja+github@gmail.com>
2019-07-01 17:46:44 +0100

    Update WaterMarkExtension.php

--- src/Extensions/WaterMarkExtension.php
+++ src/Extensions/WaterMarkExtension.php
@@ -26,7 +26,7 @@ class WaterMarkExtension extends Extension
              $config = SiteConfig::current_site_config();
    			 $wimg = clone $config->watermarkimg->getImageBackend()->getImageResource();
 			 $wimg->opacity($config->alfa);
-             $resource->insert($wimg, 'center', $clone->getWidth()*$config->posv/100, $backend->getWidth()*$config->posh/100);
+             $resource->insert($wimg, 'top-left', $clone->getWidth()*$config->posv/100, $backend->getWidth()*$config->posh/100);
              $clone->setImageResource($resource);
              
          
