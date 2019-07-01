38f2ef9c4115346ceb499cb6f7edee48f509b789
Bruno Igreja <bigreja+github@gmail.com>
2019-07-01 17:18:13 +0100

    Update WaterMarkExtension.php

--- src/Extensions/WaterMarkExtension.php
+++ src/Extensions/WaterMarkExtension.php
@@ -15,50 +15,22 @@ class WaterMarkExtension extends Extension
     
     public function WaterMark($amount = null)
     {
-        	Debug::Show($this->owner->watermarkimg);
-			Debug::Show($this->owner->watermarkimg->getWidth());
-			Debug::Show($this->owner->watermarkimg->getHeight());
-			
-            Debug::Show($this->PercentageX());
-            Debug::Show($this->PercentageY());
 			
 		$variant = $this->owner->variantName(__FUNCTION__, $amount);
         return $this->owner->manipulateImage($variant, function (\SilverStripe\Assets\Image_Backend $backend) use ($amount) {
             $clone = clone $backend;
             $resource = clone $backend->getImageResource();
             
-<<<<<<< HEAD
-             //$wimg = clone $this->owner->watermarkimg()->getImageResource();
-			 //$wimg->opacity($this->owner->alfa);
-             //$resource->insert($wimg, 'center', $this->PercentageX(), $this->PercentageY());
-             // $clone->setImageResource($resource);
-			
-             
-=======
              $config = SiteConfig::current_site_config();
    			 $wimg = clone $config->watermarkimg->Link->getImageResource();
-			 //$wimg->opacity($config->alpha);
-            $resource->insert($wimg);
+			 $wimg->opacity($config->alpha);
+             $resource->insert($wimg);
         
             $clone->setImageResource($resource, 'top-left', $config->posv, $config->posh);
->>>>>>> parent of f16c1df... Update WaterMarkExtension.php
             return $clone;
         });
     }
     
-    public function PercentageX()
-    {
-        if ($field = $this->owner->watermarkimg) {
-            return round($field->getWidth() * $this->owner->posv)/100;
-        }
-        return 0;
-    }
-    public function PercentageY()
-    {
-        if ($field = $this->owner->watermarkimg) {
-            return round($field->getHeight() * $this->owner->posh)/100;
-        }
-        return 0;
-    }
+  
 
 }
