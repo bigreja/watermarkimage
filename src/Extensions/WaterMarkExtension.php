c286ff3148ea7046c4d7375a379974e5e0841c98
Bruno Igreja <bigreja+github@gmail.com>
2019-07-01 14:22:44 +0100

    Update WaterMarkExtension.php

--- src/Extensions/WaterMarkExtension.php
+++ src/Extensions/WaterMarkExtension.php
@@ -3,40 +3,25 @@
 namespace bigreja\watermarkimage\Extensions;
 
 use SilverStripe\Core\Extension;
+use SilverStripe\SiteConfig\SiteConfig;
+
 class WaterMarkExtension extends Extension
 {
-    public function Square($width)
-    {
-        $variant = $this->owner->variantName(__FUNCTION__, $width);
-        return $this->owner->manipulateImage($variant, function (\SilverStripe\Assets\Image_Backend $backend) use($width) {
-            $clone = clone $backend;
-            $resource = clone $backend->getImageResource();
-            $resource->fit($width);
-            $clone->setImageResource($resource);
-            return $clone;
-        });
-    }
-
-    public function Blur($amount = null)
-    {
-        $variant = $this->owner->variantName(__FUNCTION__, $amount);
-        return $this->owner->manipulateImage($variant, function (\SilverStripe\Assets\Image_Backend $backend) use ($amount) {
-            $clone = clone $backend;
-            $resource = clone $backend->getImageResource();
-            $resource->blur($amount);
-            $clone->setImageResource($resource);
-            return $clone;
-        });
-    }
-
+    
+    
     public function WaterMark($amount = null)
     {
         $variant = $this->owner->variantName(__FUNCTION__, $amount);
         return $this->owner->manipulateImage($variant, function (\SilverStripe\Assets\Image_Backend $backend) use ($amount) {
             $clone = clone $backend;
             $resource = clone $backend->getImageResource();
-            $resource->insert('themes/superbraga/images/logo/sob-sb.png');
-            $clone->setImageResource($resource);
+            
+             $config = SiteConfig::current_site_config();
+   			 $wimg = $config->watermarkimg->getImageResource();
+			 $wimg->->opacity($config->alpha);
+            $resource->insert($wimg);
+        
+            $clone->setImageResource($resource, 'top-left', $config->posv, $config->posh);
             return $clone;
         });
     }
