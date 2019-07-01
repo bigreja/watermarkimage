c8619ba6e055581fcb0e4bcb3c443936c9272cbc
Bruno Igreja <bigreja+github@gmail.com>
2019-07-01 17:27:58 +0100

    Update WaterMarkExtension.php

--- src/Extensions/WaterMarkExtension.php
+++ src/Extensions/WaterMarkExtension.php
@@ -19,16 +19,15 @@ class WaterMarkExtension extends Extension
 			
 		$variant = $this->owner->variantName(__FUNCTION__, $amount);
         return $this->owner->manipulateImage($variant, function (\SilverStripe\Assets\Image_Backend $backend) use ($amount) {
-            $clone = clone $backend;
-            $resource = clone $backend->getImageResource();
+             $clone = clone $backend;
+             $resource = clone $backend->getImageResource();
             
              $config = SiteConfig::current_site_config();
    			 $wimg = clone $config->watermarkimg->getImageBackend()->getImageResource();
 			 $wimg->opacity($config->alpha);
-             $resource->insert($wimg);
-        
-            $clone->setImageResource($resource, 'top-left', $config->posv, $config->posh);
-            return $clone;
+             $resource->insert($wimg, 'center', $config->posv, $config->posh);
+             $clone->setImageResource($resource);
+             return $clone;
         });
     }
     
